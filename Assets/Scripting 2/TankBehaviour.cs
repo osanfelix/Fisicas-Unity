159215ca8b055dec571b478c139dde2e24efd9f5
Oscar Sanfelix <osanfelix@outlook.es>
2017-11-12 13:44:41 +0100

    Proyecto docente

---
+++ Assets/Scripting 2/TankBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankBehaviour : MonoBehaviour {
+
+	public float TranslateSpeed = 1f;
+	public float rotateSpeed = 1f;
+
+	protected Color _originalColor;
+
+
+	void Start()
+	{
+		Transform renderers = transform.Find("TankRenderers");
+
+		_originalColor = renderers.Find("TankTurret").GetComponent<Renderer>().material.GetColor("_Color");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.Translate(Vector3.forward * Time.deltaTime * TranslateSpeed);
+			transform.Find("TankRenderers").transform.Find("TankTurret").GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+		}
+		else if (Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.Translate(-Vector3.forward * Time.deltaTime * TranslateSpeed);
+			transform.Find("TankRenderers").transform.Find("TankTurret").GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+		}
+		else
+			transform.Find("TankRenderers").transform.Find("TankTurret").GetComponent<Renderer>().material.SetColor("_Color", _originalColor);
+
+
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
+		}
+		else if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Rotate(-Vector3.up * Time.deltaTime * rotateSpeed);
+		}
+	}
+}
