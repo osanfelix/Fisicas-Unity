4e5a92828c41b9b0202cd0c2c8da5318a65a06d1
Oscar Sanfelix <osanfelix@outlook.es>
2017-11-12 12:13:09 +0100

    Proyecto de inicio, con muchos elementos a eliminar y organizar

---
+++ Assets/Fisicas ejemplo2/light.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class light : MonoBehaviour
+{	
+	public int intensity;
+
+
+	void Start()
+	{
+		Light mi_luz = GetComponent<Light>();
+		mi_luz.color = Color.red;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+		GetComponent<Light>().intensity = (float)intensity/10;
+
+		/*
+		if(Input.GetKeyDown(KeyCode.Space))
+			GetComponent<Light>().enabled = !GetComponent<Light>().enabled;
+		*/
+	}
+}
